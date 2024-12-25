6fa65cc1177fe1c565ca09240690263a27c504c9
Alba070503 <97574964+Alba070503@users.noreply.github.com>
2024-12-24 21:29:23 -0400

    Update descargas-yt_play.js

--- plugins/descargas-yt_play.js
+++ plugins/descargas-yt_play.js
@@ -42,7 +42,7 @@ let handler = async (m, { conn, args }) => {
       m.chat,
       {
         video: buffer,
-        mimetype: "video/mp4",
+        mimetype: "audio/mp4",
         fileName: `${video.title}.mp4`,
         caption: `✰ *TÍTULO:* ${video.title}`,
       },
